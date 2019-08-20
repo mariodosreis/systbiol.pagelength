6e51a7a229bc335c6cde0b0589d6f510509a2ef7
Mario dos Reis <mariodosreis@gmail.com>
2019-08-20 21:46:56 +0100

    fix small typo in page length calculation (missing +1 in some page lengths)

--- systbiol.R
+++ systbiol.R
@@ -7,15 +7,16 @@ n <- length(df$year) # 1,495 records
 
 # calculate page lengths:
 df$last.page <- as.numeric(df$last.page)
-page.length <- df$last.page - df$first.page + 1
+page.length <- df$last.page - df$first.page
 # single page papers:
-page.length[is.na(page.length)] <- 1
+page.length[is.na(page.length)] <- 0
 # for some records, last page number my be "abbreviated"
 # in such cases last.page - first.page is negative
 i0 <- which(page.length < 0)
 page.length[i0] <- floor(df$first.page[i0] / 100) * 100 + df$last.page[i0] - df$first.page[i0]
 j0 <- which(page.length < 0)
 page.length[j0] <- floor(df$first.page[j0] / 10) * 10 + df$last.page[j0] - df$first.page[j0]
+page.length <- page.length + 1
 
 # mean of page lengths per year:
 length.mean <- aggregate(page.length, by=list(df$year), FUN=mean)
@@ -24,3 +25,9 @@ plot(df$year + rnorm(n, 0, sd=.05), page.length, pch=19, col=rgb(0,0,1,.1),
      xlab="Year of Publication", ylab="Page length of Systematic Biology papers", las=1)
 lines(length.mean, lwd=2)
 
+# Syst. Biol. longest papers:
+df[which(page.length > 40),]
+# x     pmid year month first.page last.page
+# 1237 PMID- 12118410 2000   Jun        306        62
+# 1465 PMID- 11975331 1997   Sep        479       522
+# 1494 PMID- 11975347 1997   Mar          1        68
